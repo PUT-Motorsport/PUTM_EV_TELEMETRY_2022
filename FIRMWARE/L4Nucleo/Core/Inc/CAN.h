a96f662305add6481df027ac5f637b4cff4bf68d
wasyl07 <wasyl6610@gmail.com>
2022-03-29 11:41:53 +0200

    CAN Testing

---
+++ FIRMWARE/L4Nucleo/Core/Inc/CAN.h
@@ -0,0 +1,17 @@
+/*
+ * CAN.h
+ *
+ *  Created on: Feb 3, 2022
+ *      Author: Adam Wasilewski
+ */
+
+#ifndef INC_CAN_H_
+#define INC_CAN_H_
+
+#include "stm32l4xx_hal.h"
+
+void Open_Filter(void);
+void Close_Filter(void);
+void CAN_Init(void);
+
+#endif /* INC_CAN_H_ */
